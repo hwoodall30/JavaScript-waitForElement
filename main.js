4eac19ea9f3a8f18da0d2d78f3cc621aaf658d1c
hwoodall30 <75491683+hwoodall30@users.noreply.github.com>
2022-06-18 21:20:15 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+async function waitForElement(selector, timeout = 5000, interval = 0) {
+	// Request Animation Frame Async
+	function rafAsync() {
+		return new Promise(resolve => requestAnimationFrame(resolve));
+	}
+	// Sleep Async Function
+	const sleep = () => new Promise(r => setTimeout(r, interval));
+	//Get start time for timeout
+	const start = Date.now();
+	// Loop until element is found or timeout is reached
+	while (document.querySelector(selector) === null) {
+		if (Date.now() - start >= timeout) {
+			console.error(`Element Check For Selector ${selector} Timed Out`);
+			return false;
+		}
+		if (interval) await sleep();
+		await rafAsync();
+	}
+	return document.querySelector(selector);
+}
